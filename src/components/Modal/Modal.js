8ba28154aa4cdfe139f932f405aed85a3df33a55
Evgeniy Yurchak <zhenyaa2007@gmail.com>
2021-08-14 00:49:11 +0300

    edited

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './Modal.module.css';
+import { createPortal } from 'react-dom';
+
+const modalRoot = document.querySelector('#modal-root');
+
+class Modal extends Component {
+  state = {};
+
+  static propTypes = {
+    toggleModal: PropTypes.func.isRequired,
+    children: PropTypes.node.isRequired,
+  };
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleEscape);
+  }
+
+  handleEscape = event => {
+    if (event.code === 'Escape') {
+      this.props.toggleModal();
+    }
+  };
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleEscape);
+  }
+
+  handleBackdrop = event => {
+    if (event.target === event.currentTarget) {
+      this.props.toggleModal();
+    }
+  };
+
+  render() {
+    return createPortal(
+      <div className={styles.backdrop} onClick={this.handleBackdrop}>
+        <div className={styles.content}>{this.props.children}</div>
+      </div>,
+      modalRoot,
+    );
+  }
+}
+
+export default Modal;
